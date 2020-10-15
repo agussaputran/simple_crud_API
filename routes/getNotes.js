3c33e854b20c3a5029bd6df3668319242ed419ac
agussaputran <ntoiagussaputra@gmail.com>
2020-10-15 10:20:08 +0700

    add single note handler

--- routes/getNotes.js
+++ routes/getNotes.js
@@ -4,8 +4,14 @@ const app = express.Router();
 const db = require("../controller/dbController");
 
 app.get("/notes", (req, res) => {
-  let result = db.Get();
-  res.send(result);
+  const id = req.query.id;
+  const parsedId = parseInt(id);
+  if (id) {
+    res.send(db.GetId(parsedId));
+  } else {
+    let result = db.Get();
+    res.send(result);
+  }
 });
 
 module.exports = app;
